2e002299197ac8733b3051cee60f420e887d17fa
OSCAR ANDRES CESPEDES DIAZ <oscar.cespedes@correo.policia.gov.co>
2018-07-24 20:54:22 -0500

    creacion_relaciones_campos

---
+++ models/friends.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+module.exports = (sequelize, DataTypes) => {
+    let Friends = sequelize.define('Friends', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }, {
+            // don't add the timestamp attributes (updatedAt, createdAt)
+            timestamps: true,
+
+            // don't use camelcase for automatically added attributes but underscore style
+            // so updatedAt will be updated_at
+            underscored: true,
+
+            // disable the modification of table names; By default, sequelize will automatically
+            // transform all passed model names (first parameter of define) into plural.
+            // if you don't want that, set the following
+            freezeTableName: true,
+            
+            // define the table's name
+            tableName: 'friends',
+
+            // Enable optimistic locking.  When enabled, sequelize will add a version count attribute
+            // to the model and throw an OptimisticLockingError error when stale instances are saved.
+            // Set to true or a string with the attribute name you want to use to enable.
+            version: false
+        });
+
+        //Relacion con User
+    return Friends;
+};
